51f81021b47cb5b6936c7122acb28eb136dc1802
Rune Jeppesen <run@sdu.dk>
2023-08-30 11:37:42 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import { useState, useEffect } from "react";
 import { checkValue } from "./utils";
 import { config } from "./config";
 import Task from "./task";
-// import { createClient } from "@supabase/supabase-js";
-// const supabaseUrl = "https://cmqxnlontexbrcgilpqs.supabase.co";
-// const supabase = createClient(
-//   supabaseUrl,
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtcXhubG9udGV4YnJjZ2lscHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDgyMzgxMjYsImV4cCI6MTk2MzgxNDEyNn0.r-fRpNtLRnGUIji-sAu2ecAY-d635SsGHS08Va5-u20"
-// );
-let channel = null;
 export default () => {
   let paramString = window.location.search.split("?")[1];
   let queryString = new URLSearchParams(paramString);
@@ -26,42 +19,9 @@ export default () => {
       // }),
       false
     );
-    // channel = supabase.channel("online-users", {
-    //   config: {
-    //     presence: {
-    //       key: Math.random() + "x",
-    //     },
-    //   },
-    // });
-
-    // channel.on("presence", { event: "sync" }, () => {
-    //   console.log("Online users: ", channel.presenceState());
-    // });
-
-    // channel.on("presence", { event: "join" }, ({ newPresences }) => {
-    //   console.log("New users have joined: ", newPresences);
-    // });
-
-    // channel.on("presence", { event: "leave" }, ({ leftPresences }) => {
-    //   console.log("Users have left: ", leftPresences);
-    // });
-
-    // channel.subscribe(async (status) => {
-    //   if (status === "SUBSCRIBED") {
-    //     const status = await channel.track({
-    //       online_at: new Date().toISOString(),
-    //     });
-    //     console.log(status);
-    //   }
-    // });
   }, []);
   useEffect(() => {
     console.log({ route, currentTaskId });
-    // channel.track({
-    //   online_at: new Date().toISOString(),
-    //   route,
-    //   currentTaskId,
-    // });
   }, [route, currentTaskId]);
   const { tasks, rootImageFileName, dynamicImages } = config;
   const taskClick = (e, id) => {
--- src/task.tsx
+++ src/task.tsx
@@ -22,29 +22,42 @@ export default (props) => {
         return !checkValue(answer, requirement);
       }
     );
-    if (allAnsweredOk) answerClick(newAnswers);
-    else setAnswers(newAnswers);
+    //if (allAnsweredOk) answerClick(newAnswers);
+    //else
+    setAnswers(newAnswers);
   };
   const closeClick = () => {
     setCurrentTaskId(null);
   };
-  const answerClick = (newAnswers) => {
+  const answerClick = () => {
     const r = { ...route };
-    newAnswers.forEach((answer, index) => {
+    answers.forEach((answer, index) => {
       r[currentTaskId + index] = answer;
     });
-
-    let url = location.origin + "/?";
-    Object.entries(r).forEach(([id, answer]) => {
-      url += id + "=" + answer + "&";
-    });
-    setCurrentTaskId(null);
-    setRoute(r);
-    history.pushState(null, "", url.substring(0, url.length - 1));
+    const allAnsweredOk = !acceptableAnswers.find(
+      (requirement, requirementIndex) => {
+        const answer = answers[requirementIndex];
+        if (answer === undefined) return true;
+        return !checkValue(answer, requirement);
+      }
+    );
+    if (allAnsweredOk) {
+      const url =
+        location.origin +
+        "/?" +
+        Object.entries(r)
+          .map(([id, answer]) => id + "=" + answer)
+          .join("&");
+      setCurrentTaskId(null);
+      setRoute(r);
+      history.pushState(null, "", url);
+    } else {
+      //showhelp
+    }
   };
-  const answerCount = answers.filter(
-    (answer) => answer && answer.length
-  ).length;
+  // const answerCount = answers.filter(
+  //   (answer) => answer && answer.length
+  // ).length;
   console.log(route);
   let secondImageComponent = null;
   if (secondImage) {
@@ -89,7 +102,7 @@ export default (props) => {
       <button
         type="button"
         onClick={closeClick}
-        class="absolute left-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        className="absolute left-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
       >
         Hjælp
       </button>
@@ -98,11 +111,10 @@ export default (props) => {
           <button
             type="button"
             onClick={closeClick}
-            class="absolute right-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            className="fixed right-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
           >
             Annuller
           </button>
-
           {image && <img className="pt-10" src={"/" + image}></img>}
         </div>
         <br />
@@ -134,6 +146,17 @@ export default (props) => {
           );
         })}
         {secondImageComponent}
+        <div className="sm:w-1 md:w-1/2 ">
+          <div className="relative">
+            <button
+              type="button"
+              onClick={answerClick}
+              className="absolute right-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              Angiv svar
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
